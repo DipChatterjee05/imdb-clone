9b2f7c82a0c92155f38b652a155b1a2337d103e9
DipChatterjee05 <jerrychatterjee55@gmail.com>
2024-01-27 21:21:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+    <Header />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/movie/:id" element={<h1>Movie Detail Page</h1>} />
+        <Route path="/movies/:type" element={<h1>Movie List Page</h1>} />
+        <Route path="/*" element={<h1>Error 404!!!</h1>} />
+      </Routes>
     </div>
   );
 }
