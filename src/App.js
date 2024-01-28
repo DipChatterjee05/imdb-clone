099d93e686dc0cb28aa241bef34e9c4493550171
DipChatterjee05 <jerrychatterjee55@gmail.com>
2024-01-28 19:06:52 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Home from "./pages/Home/Home";
+import MovieList from "./components/MovieList/MovieList";
+import MovieDetail from "./pages/MovieDetail/MovieDetail";
 
 function App() {
   return (
     <div className="App">
-    <Header />
+      <Header />
       <Routes>
         <Route index element={<Home />} />
-        <Route path="/movie/:id" element={<h1>Movie Detail Page</h1>} />
-        <Route path="/movies/:type" element={<h1>Movie List Page</h1>} />
+        <Route path="/movie/:id" element={<MovieDetail />} />
+        <Route path="/movies/:type" element={<MovieList />} />
         <Route path="/*" element={<h1>Error 404!!!</h1>} />
       </Routes>
     </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import "./Home.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
+import { Link } from "react-router-dom";
+import MovieList from "../../components/MovieList/MovieList";
 
 const Home = () => {
     const [popularMovies, setPopularMovies] = useState([]);
@@ -17,12 +19,26 @@ const Home = () => {
             <Carousel showThumbs={false} autoPlay={true} transitionTime={3} infiniteLoop={true} showStatus={false}>
                 {
                     popularMovies.map(movie => (
-                        <div className="posterImage">
-                            <img src={`https://image.tmdb.org/t/p/original${movie && movie.backdrop_path}`} alt="movie-poster" />
-                        </div>
+                        <Link style={{ textDecoration: "none", color: "white" }} to={`/movie/${movie.id}`} >
+                            <div className="posterImage">
+                                <img src={`https://image.tmdb.org/t/p/original${movie && movie.backdrop_path}`} alt="movie-poster" />
+                            </div>
+                            <div className="posterImage__overlay">
+                                <div className="posterImage__title">{movie ? movie.original_title : ""}</div>
+                                <div className="posterImage__runtime">
+                                    {movie ? movie.release_date : ""}
+                                    <span className="posterImage__rating">
+                                        <i className="fas fa-star" />{" "}
+                                        {movie ? movie.vote_average.toFixed(1) : ""}
+                                    </span>
+                                </div>
+                                <div className="posterImage__description">{movie ? movie.overview : ""}</div>
+                            </div>
+                        </Link>
                     ))
                 }
             </Carousel>
+            <MovieList />
         </div>
     );
 }
